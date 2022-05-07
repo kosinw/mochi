d6c2b42a9e621195bf432b692140a25e5126158c
AnirudhRahul <anirudhrahul@anjurahul.com>
2022-05-06 21:33:59 -0400

    Changed flour names

--- src/flour/index.ts
+++ src/flour/index.ts
@@ -1,3 +1,4 @@
 export * from './object';
+export { FlourBoxedTypeCode as BoxedValueVariant, FlourUnboxedTypeCode as UnboxedValueVariant } from "./typecode";
 export * from './opcode';
 export * from './serde';
