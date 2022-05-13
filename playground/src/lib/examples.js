be2c8460b294568e7702015b0a5009eac5324d2c
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-05-13 11:46:28 -0400

    updated examples

--- playground/src/lib/examples.js
+++ playground/src/lib/examples.js
@@ -84,5 +84,70 @@ export default {
 
 (map print perfect-squares)`,
 
+  folding: `(define cons
+  (lambda (x y)
+    (lambda (msg)
+      (if (= msg 0) x y))))
+
+(define car
+  (lambda (pair)
+    (pair 0)))
+
+(define cdr
+  (lambda (pair)
+    (pair 1)))
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define cadr
+  (compose car cdr))
+
+(define (foldl kons a lst)
+  (if (null? lst)
+    a
+    (foldl kons 
+      (kons a (car lst))
+      (cdr lst))))
+
+(define (foldr kons a lst)
+  (if (null? lst)
+    a
+    (kons (car lst)
+          (foldr kons a (cdr lst)))))
+
+(define (map fun lst)
+  (foldr (lambda (x a) (cons (fun x) a)) () lst))
+
+(define (xcons car cdr)
+  (cons cdr car))
+
+(define (reverse lst)
+  (foldl xcons () lst))
+
+(define (for-each fun lst)
+  (map 
+    (lambda (x) (begin (fun x)))
+    (reverse lst))
+  ())
+
+(define (filter fun lst)
+  (foldr 
+    (lambda (x a)
+      (if (fun x)
+        (cons x a)
+        a))
+    () lst))
+
+(define the-list (cons 2 (cons 3 (cons 4 (cons 5 ())))))
+
+(define (square x) (* x x))
+
+(define perfect-squares (map square the-list))
+
+(for-each print perfect-squares)
+(for-each print (reverse perfect-squares))`,
+
   empty: ""
 };
